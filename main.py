da764aa6e8b2153725b7663e097bf1de796c1b15
soupday <79094830+soupday@users.noreply.github.com>
2022-05-12 04:49:15 +0100

    1.0.6
    -

--- main.py
+++ main.py
@@ -14,8 +14,6 @@
 # You should have received a copy of the GNU General Public License
 # along with CC4-Blender-Tools-Plugin.  If not, see <https://www.gnu.org/licenses/>.
 
-from msilib.schema import MsiAssembly
-from sys import api_version
 import RLPy
 import json
 import os
