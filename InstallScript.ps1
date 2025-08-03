ecabf55af3c5590a8d868bd50912864663b4184b
soupday <79094830+soupday@users.noreply.github.com>
2025-08-03 09:48:19 +0100

    2.3.2
    Install/Uninstall script

---
+++ InstallScript.ps1
@@ -0,0 +1,103 @@
+# elevation handled by the calling .bat file to allow script execution policy override
+<#
+param([switch]$elevated)
+
+function Test-Admin {
+    $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+    $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+if ((Test-Admin) -eq $false)  {
+    if ($elevated) {
+        # tried to elevate, did not work, aborting
+    } else {
+        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+    }
+    exit
+}
+#>
+
+# registry vars
+$iCloneKeyPath = "HKLM:\SOFTWARE\Reallusion\iClone\8.0"
+$charCreatorKeyPath = "HKLM:\SOFTWARE\Reallusion\Character Creator\4.0"
+$charCreatorFiveKeyPath = "HKLM:\SOFTWARE\Reallusion\Character Creator\5.0"
+$valueName = "Install Path"
+
+# path vars
+$subFolder = "OpenPlugin"
+
+# script vars
+$scriptFolder = $PSScriptRoot
+#$scriptFolderName = Split-Path -Path $scriptFolder -Leaf
+$scriptFolderName = "Blender Pipeline Plugin"
+
+function Add-Junction($keyPath){
+    $keyExists = Test-Path -Path $keyPath
+    if($keyExists){
+        Write-Host "`n Found $keyPath in the registry."
+
+        # check if the entry "Install Path" is present within the key
+        $entryExists = Get-ItemProperty -Path $keyPath -Name $valueName -ErrorAction SilentlyContinue
+
+        if ($entryExists){
+            $entryValue = Get-ItemPropertyValue -Path $keyPath -Name $valueName
+
+            # check if subfolder is present under the install path
+            $subFolderExists = Test-Path -Path "$entryValue\$subFolder"
+
+            if($subFolderExists){
+                Write-Host "`n $subFolder folder exists (in $entryValue)."
+            }else{
+                Write-Host "`n Creating $subFolder folder in $entryValue."
+                New-Item -Path "$entryValue\$subFolder" -ItemType Directory
+            }
+
+            # is this a Git Clone? Install as junction, otherwise install as full copy
+            $isGitClone = Test-Path -Path "$scriptFolder\.git"
+            if ($isGitClone){
+                Write-Host "`n Installing Git Clone Plugin as junction link."
+            }else{
+                Write-Host "`n Installing Plugin Files directly."
+            }
+
+            # create a directory junction to the folder containing this script
+            $junctionPath = "$entryValue\$subFolder\$scriptFolderName"
+
+            $junctionExists = Test-Path -Path $junctionPath
+            if($junctionExists){
+                Write-Host "`n Folder link $junctionPath already exists."
+                cmd /c rmdir /s /q $junctionPath
+                Write-Host "`n Folder link $junctionPath deleted."
+            }
+            $junctionExists = Test-Path -Path $junctionPath
+            if(!$junctionExists){
+                if ($isGitClone){
+                    cmd /c mklink /J "$junctionPath" "$scriptFolder" | Out-Null
+                    Write-Host "`n Folder link $junctionPath has been created."
+                }else{
+                    Copy-Item -Path "$scriptFolder" -Destination "$junctionPath" -Recurse
+                    Write-Host "`n Folder $junctionPath has been copied."
+                }
+            }
+        }else{
+            Write-Host "`n Cannot find $valueName in the registry key... skipping."
+        }
+    }else{
+        Write-Host "`n Cannot find $keyPath in the registry... skipping."
+    }
+}
+
+Write-Host "`n************************************************************************"
+Write-Host "`n This script will create links that allow iClone/Character Creator`n to use the plugin contained in this folder - Please ensure that this`n folder stays in its current location (or the links will break).`n`n [If you need to move the folder then run the Uninstall.bat file before`n re-running the Install.bat file.]"
+Write-Host "`n************************************************************************`n"
+
+$keyPaths = @($iCloneKeyPath, $charCreatorKeyPath, $charCreatorFiveKeyPath)
+
+foreach ($entry in $keyPaths){
+    Write-Host "`n`n Processing $entry ..."
+
+    Add-Junction($entry)
+}
+
+Write-Host "`n`n Process Complete - Press any key to exit...`n"
+$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
---
+++ UnInstall.bat
@@ -0,0 +1,6 @@
+:: https://blog.danskingdom.com/allow-others-to-run-your-powershell-scripts-from-a-batch-file-they-will-love-you-for-it/
+@ECHO OFF
+SET pwd=%~dp0
+SET PSScriptPath=%pwd%UnInstallScript.ps1
+:: PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '%PSScriptPath%'";
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%PSScriptPath%""' -Verb RunAs}";
